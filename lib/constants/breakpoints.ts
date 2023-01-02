258715738f17b9615465cc4d0be7d5c2546c8b67
Charlie <68297416+charles01pd2018@users.noreply.github.com>
2023-01-02 11:59:24 -0800

    footer mobile fix

--- lib/constants/breakpoints.ts
+++ lib/constants/breakpoints.ts
@@ -2,4 +2,4 @@ export const BREAKPOINT_EXTRA_LARGE = 1250;             // anything under is not
 export const BREAKPOINT_LARGE = 1025;                   // anything under is not a desktop   
 export const BREAKPOINT_MEDIUM = 800;                   // anything under is not a tablet
 export const BREAKPOINT_SMALL = 600;                    // anything is under is a mobile device
-export const BREAKPOINT_EXTRA_SMALL = 500;              // anything is under is a mobile device
+export const BREAKPOINT_EXTRA_SMALL = 450;              // anything is under is a mobile device
