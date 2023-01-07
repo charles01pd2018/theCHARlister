2fdc45b6313d83c0be4ce32c5fe25b3e820ecaa7
Charlie <68297416+charles01pd2018@users.noreply.github.com>
2023-01-06 21:51:49 -0800

    Fix arrow color

--- elements/icon/animated/AnimatedCurveArrowIcon.tsx
+++ elements/icon/animated/AnimatedCurveArrowIcon.tsx
@@ -21,6 +21,7 @@ const AnimatedCurveArrowIcon = ( {
 }: Props ) => {
     /* CONTENT */
     const stroke = COLORS[ color ][ 4 ];
+    const markerId = `SvgjsMarker-${color}`;
 
     /* CLASSNAMES */
     const animatedCurveArrowIconClasses = classNames(
@@ -31,24 +32,24 @@ const AnimatedCurveArrowIcon = ( {
     );
 
     return (
-        <svg className={animatedCurveArrowIconClasses} viewBox="0 0 800 800"
-            xmlns="http://www.w3.org/2000/svg">
-            <g strokeWidth="12" stroke={stroke} fill="none" 
-                strokeLinecap="round" strokeLinejoin="round">
+        <svg className={animatedCurveArrowIconClasses} viewBox='0 0 800 800'
+            xmlns='http://www.w3.org/2000/svg'>
+            <g strokeWidth='12' stroke={stroke} fill='none' 
+                strokeLinecap='round' strokeLinejoin='round'>
                 <path className='line-path'
-                    d="M250 250Q450 350 400 400Q294 533 550 550 " 
-                    markerEnd="url(#SvgjsMarker1141)" />
+                    d='M250 250Q450 350 400 400Q294 533 550 550' 
+                    markerEnd={`url(#${markerId})`} />
             </g>
             <defs>
                 <marker className='arrow-tip'
-                    markerWidth="10" markerHeight="10" refX="5" refY="5" 
-                    viewBox="0 0 10 10" orient="auto" id="SvgjsMarker1141">
-                    <polyline points="0,5 5,2.5 0,0" fill="none" 
-                        strokeWidth="1.6666666666666667" 
+                    markerWidth='10' markerHeight='10' refX='5' refY='5' 
+                    viewBox='0 0 10 10' orient='auto' id={markerId}>
+                    <polyline points='0,5 5,2.5 0,0' fill='none' 
+                        strokeWidth='1.6666666666666667' 
                         stroke={stroke}
-                        strokeLinecap="round" 
-                        transform="matrix(1,0,0,1,1.6666666666666667,2.5)" 
-                        strokeLinejoin="round" />
+                        strokeLinecap='round' 
+                        transform='matrix(1,0,0,1,1.6666666666666667,2.5)' 
+                        strokeLinejoin='round' />
                 </marker>
             </defs>
         </svg>
