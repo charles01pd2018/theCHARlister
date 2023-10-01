2a71e6ccdc287dddb2334ec983fc9d58e26f9390
Charlie <68297416+charles01pd2018@users.noreply.github.com>
2023-09-30 22:27:00 -0700

    update content

--- components/experience-list/content.ts
+++ components/experience-list/content.ts
@@ -4,15 +4,62 @@ export const EXPERIENCE_LIST_CONTENT: ExperienceListContent = {
     items: [
         {
             logo: {
+                data: '/static/icons/avery-logo.svg',
+                alt: 'Avery Logo',
+            },
+            company: {
+                text: 'Avery',
+                href: 'https://app.print.avery.com/dpo8',
+            },
+            role: 'Software Developement Engineer I',
+            dateRange: 'Oct 2022 - Present',
+            technologies: {
+                icons: [
+                    {
+                        data: '/static/icons/angular-bg-logo.svg',
+                        alt: 'Angular Logo',
+                        title: 'Angular',
+                    },
+                    {
+                        data: '/static/icons/backbonejs-bg-logo.svg',
+                        alt: 'BackboneJS Logo',
+                        title: 'BackboneJS',
+                    },
+                    {
+                        data: '/static/icons/sass-bg-logo.svg',
+                        alt: 'Sass Logo',
+                        title: 'Sass',
+                    },
+                    {
+                        data: '/static/icons/nodejs-bg-logo.svg',
+                        alt: 'NodeJS Logo',
+                        title: 'NodeJS',
+                    },
+                    {
+                        data: '/static/icons/storybook-bg-logo.svg',
+                        alt: 'Storybook Logo',
+                        title: 'Storybook',
+                    },
+                ],
+            },
+            bullets: [
+                'Leading the migration to Google Analytics 4, communicating with key stakeholders and 3rd party vendor to ensure optimal data transfer of 50+ hard-coded events',
+                'Contributed to the launch of the new user interface for the Digital Print Online application - addressing stories and defects relating to virtual lists, resizable popups, inputs, etc.',
+                'Documented and created a standard for localization for 1500+ locale keys ',
+                'Collaborated with the software and design team to build out new UI components consisting of new dialog boxes, radio buttons, numbered lists, etc. as well as documented components within Storybook'
+            ],
+        },
+        {
+            logo: {
                 data: '/static/icons/knowt-logo.svg',
                 alt: 'Knowt Logo',
             },
             company: {
                 text: 'Knowt',
-                href: 'https://knowt.io/',
+                href: 'https://knowt.com/',
             },
             role: 'Software Engineer',
-            dateRange: 'Oct 2022 - Present',
+            dateRange: 'Oct 2022 - June 2023',
             technologies: {
                 icons: [
                     {
@@ -55,52 +102,6 @@ export const EXPERIENCE_LIST_CONTENT: ExperienceListContent = {
                 'Fixed multiple note-editor bugs consisting of ✨ markdown parsing, saving, custom key inputs, paste parsing, image resizing + rendering + reordering, list conversions for nested lists, node popup selections, and google drive imports',
                 'Created a better saving experience by enabling <b><u>manual shortcut saving</u></b> (command+S) in combination with debounce saving for both the note and flashcard editor',
                 'Contributed to the migration of Python backend to Typescript and increased server speed from 🧑‍💻 O(n2) → O(n) through code optimization'
-            ],
-        },
-        {
-            logo: {
-                data: '/static/icons/avery-logo.svg',
-                alt: 'Avery Logo',
-            },
-            company: {
-                text: 'Avery',
-                href: 'https://www.avery.com/',
-            },
-            role: 'Software Developement Engineer I',
-            dateRange: 'Oct 2022 - Present',
-            technologies: {
-                icons: [
-                    {
-                        data: '/static/icons/angular-bg-logo.svg',
-                        alt: 'Angular Logo',
-                        title: 'Angular',
-                    },
-                    {
-                        data: '/static/icons/backbonejs-bg-logo.svg',
-                        alt: 'BackboneJS Logo',
-                        title: 'BackboneJS',
-                    },
-                    {
-                        data: '/static/icons/sass-bg-logo.svg',
-                        alt: 'Sass Logo',
-                        title: 'Sass',
-                    },
-                    {
-                        data: '/static/icons/nodejs-bg-logo.svg',
-                        alt: 'NodeJS Logo',
-                        title: 'NodeJS',
-                    },
-                    {
-                        data: '/static/icons/storybook-bg-logo.svg',
-                        alt: 'Storybook Logo',
-                        title: 'Storybook',
-                    },
-                ],
-            },
-            bullets: [
-                'Collaborated with the software and design team to build out new UI components consisting of new dialog boxes, radio buttons, numbered lists, etc. as well as documented those components within Storybook',
-                'Troubleshooted authentication event tracking with Marketing team, manager and third-party technical representative',
-                'Made several updates to our internal tools that correspond to business needs',
             ],
         },
     ],
