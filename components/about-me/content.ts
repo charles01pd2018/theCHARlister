1abdd23ae954f0cf111d172b5bf56ef24f69c57d
Charlie <68297416+charles01pd2018@users.noreply.github.com>
2023-09-30 22:38:15 -0700

    svg styling

--- components/about-me/content.ts
+++ components/about-me/content.ts
@@ -13,42 +13,42 @@ export const ABOUT_ME_CONTENT: AboutMeContent = {
     paletteContent: {
         icons: [
             {
-                data: 'static/icons/react-logo.svg',
+                data: '/static/icons/react-logo.svg',
                 alt: 'React Logo',
                 title: 'React',
             },
             {
-                data: 'static/icons/nextjs-logo.svg',
+                data: '/static/icons/nextjs-logo.svg',
                 alt: 'NextJS Logo',
                 title: 'NextJS',
             },
             {
-                data: 'static/icons/sass-logo.svg',
+                data: '/static/icons/sass-logo.svg',
                 alt: 'Sass Logo',
                 title: 'Sass',
             },
             {
-                data: 'static/icons/typescript-logo.svg',
+                data: '/static/icons/typescript-logo.svg',
                 alt: 'Typescript Logo',
                 title: 'Typescript',
             },
             {
-                data: 'static/icons/nodejs-logo.svg',
+                data: '/static/icons/nodejs-logo.svg',
                 alt: 'NodeJS Logo',
                 title: 'NodeJS',
             },
             {
-                data: 'static/icons/html-logo.svg',
+                data: '/static/icons/html-logo.svg',
                 alt: 'HTML Logo',
                 title: 'HTML',
             },
             {
-                data: 'static/icons/mongodb-logo.svg',
+                data: '/static/icons/mongodb-logo.svg',
                 alt: 'MongoDB Logo',
                 title: 'MongoDB',
             },
             {
-                data: 'static/icons/graphql-logo.svg',
+                data: '/static/icons/graphql-logo.svg',
                 alt: 'GraphQL Logo',
                 title: 'GraphQL',
             },
