94d008e193ae44bdc640dfa7b9819192db31adb6
Charlie <68297416+charles01pd2018@users.noreply.github.com>
2023-10-08 15:56:06 -0700

    Update content.ts

--- components/experience-list/content.ts
+++ components/experience-list/content.ts
@@ -45,8 +45,8 @@ export const EXPERIENCE_LIST_CONTENT: ExperienceListContent = {
             bullets: [
                 'Leading the migration to Google Analytics 4, communicating with key stakeholders and 3rd party vendor to ensure optimal data transfer of 50+ hard-coded events',
                 'Contributed to the launch of the new user interface for the Digital Print Online application - addressing stories and defects relating to virtual lists, resizable popups, inputs, etc.',
-                'Documented and created a standard for localization for 1500+ locale keys ',
-                'Collaborated with the software and design team to build out new UI components consisting of new dialog boxes, radio buttons, numbered lists, etc. as well as documented components within Storybook'
+                'Documented and created a standard on localization for 1500+ locale keys ',
+                'Collaborated with the software and design team to build out new UI components consisting of new dialog boxes, radio buttons, numbered lists, etc. as well as documented those components within Storybook'
             ],
         },
         {
@@ -95,9 +95,9 @@ export const EXPERIENCE_LIST_CONTENT: ExperienceListContent = {
                 ],
             },
             bullets: [
-                'Acted as a key participant in the launch of <b><u>Knowt AI</u></b> by enabling dynamic positioning of the AI Input Menu and custom handlers to manipulate existing editor data',
+                'Ideated and implemented product flows for <b>Knowt AI</b> assistant to create flashcards and notes with <b>generative AI</b> through custom prompts, resulting in hundreds of paid user signups over 3 months',
                 'Led one of the first revenue sources through <b><u>Google Adsense</b></u> and Amazon Associates display advertisements - configuring ads in both a testing and production environment',
-                'Launched several key note-editor features including a new <b><u>toolbar</b></u> look with shortcut tooltips, <mark class="pink">background marks</mark>, and a resizable navigation panel',
+                'Launched several key note-editor features including a new <b><u>toolbar</b></u> look with shortcut tooltips, <mark class="pink">background marks</mark>, and a resizable navigation panel - creating a 30% lift in new notes created over the course of 2 months after launch',
                 'Repurposed existing note-editor to launch the new <b><u>flashcard editor</u></b> with custom ✨ markdown parsing',
                 'Fixed multiple note-editor bugs consisting of ✨ markdown parsing, saving, custom key inputs, paste parsing, image resizing + rendering + reordering, list conversions for nested lists, node popup selections, and google drive imports',
                 'Created a better saving experience by enabling <b><u>manual shortcut saving</u></b> (command+S) in combination with debounce saving for both the note and flashcard editor',
